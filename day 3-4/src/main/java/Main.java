6893901d029ecaab680533dc0369a4ed4db5130e
ahremenkoartem <ahremenkoartem@mail.ru>
2019-10-24 14:06:57 +0700

    day4

--- day 3-4/src/main/java/Main.java
+++ day 3-4/src/main/java/Main.java
@@ -1,7 +1,9 @@
 package main.java;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         RepeatedWordReader repeatedWordReader = new RepeatedWordReader("C:\\Users\\user\\IdeaProjects\\day 3-4\\src\\main\\resources\\test");
         repeatedWordReader.count();
     }
--- day 3-4/src/main/java/RepeatedWordReader.java
+++ day 3-4/src/main/java/RepeatedWordReader.java
@@ -1,11 +1,10 @@
 package main.java;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class RepeatedWordReader {
     final String filename;
@@ -13,33 +12,55 @@ public class RepeatedWordReader {
     public RepeatedWordReader(String filename) {
         this.filename = filename;
     }
-    public void count(){
+    @SuppressWarnings("unchecked")
+    public void count() {
         try(final BufferedReader reader = new BufferedReader(new FileReader(filename))){
             String currentLine;
+            int character;
+            StringBuffer sb = new StringBuffer();
             String mostRepeatedWord = null;
-            final HashMap<String,Integer> wordCountMap = new HashMap<>();
+            final Map<String,Integer> wordCountMap = new HashMap<>();
+
             int count = 0;
             int tempCount = 0;
-            while ((currentLine=reader.readLine())!=null){
-                String[] words = currentLine.toLowerCase().split("[.,;:\\s]");
-                for(String word: words){
+            while ((character=reader.read())!=-1){
+//                String[] words = currentLine.toLowerCase().split("[.,;:\\s]");
+
+                if(((char) character) != ' ' && ((char) character) != '.' && ((char) character) != ';'
+                        &&((char) character) != ':' &&
+                        character != 10 && character != 13){
+                    sb.append((char)character);
+                    continue;
+                }
+                String word = sb.toString().toLowerCase();
+
+                sb.delete(0,sb.length());
+//                sb.append(character);
+//                for(String word: words){
                     if(word.length()==0){
                         continue;
                     }
                     tempCount = wordCountMap.containsKey(word)?wordCountMap.get(word)+1:1;
                     if(tempCount>count){
                         count=tempCount;
-                        mostRepeatedWord =word;
                     }
                     wordCountMap.put(word,tempCount);
                 }
+            final  Set<Map.Entry<String,Integer>> entries = wordCountMap.entrySet();
+            final List<Map.Entry<String,Integer>> sortedList = new ArrayList<>(wordCountMap.entrySet());
+        sortedList.sort(new Comparator<Map.Entry<String,Integer>>() {
+                @Override
+                public int compare(Map.Entry<String,Integer> o1,Map.Entry<String,Integer> o2){
+                    return (o2.getValue()).compareTo(o1.getValue());
+                }
+            });
+            for (Map.Entry<String,Integer> entry: sortedList){
+                if (entry.getValue()>=count){
+                    System.out.println("The most repeated word in input file is: "+entry.getKey());
+                    break;
+                }
             }
-            final Set<Map.Entry<String, Integer>> entrySet = wordCountMap.entrySet();
-            System.out.println("The most repeated word in input file is: "+mostRepeatedWord);
-
             System.out.println("Number of occurences :"+ count);
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        }catch (IOException e){}
     }
 }
